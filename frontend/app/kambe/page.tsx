b270ddb4930a0e17f67e2bd059872683ed0d3439
roxymigurdia78 <yoshifumikurosu1547@gmail.com>
2026-01-28 13:30:12 +0900

    login,logout大まかに実装、バックエンド側未実装

--- frontend/app/kambe/page.tsx
+++ frontend/app/kambe/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+// app/kambe/page.tsx
+import { auth, signOut } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function KambePage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <div>
-     print("ログイン成功してるかもー")
+      //test用です、ログアウトボタン用意しました。
+      <form
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: "/login" });
+        }}
+      >
+        <button type="submit">ログアウト</button>
+      </form>
     </div>
   );
 }
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,64 +1,41 @@
-import { signIn, auth, signOut } from "@/auth";
-//import Image from "next/image";
+import { signIn, auth } from "@/auth";
 import styles from "./login.module.css";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
 
+  if (session) {
+    redirect("/kambe");
+  }
+
   return (
     <div className={styles.loginWrapper}>
-      {session !== null ? (
-        <div className={styles.loggedIn}>
-          <h1>{session.user?.name}がログインしたよ</h1>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+      >
+        <button type="submit" className={styles.gsiMaterialButton}>
+          <div className={styles.gsiMaterialButtonState}></div>
 
-          {session.user?.image && (
-            <img
-              src={session.user.image}
-              alt="user image"
-              width={80}
-              height={80}
-              className={styles.avatar}
-            />
-          )}
-
-          <form
-            action={async () => {
-              "use server";
-              await signOut();
-            }}
-          >
-            <button type="submit" className={styles.signOutButton}>
-              Sign out
-            </button>
-          </form>
-        </div>
-      ) : (
-        <form
-          action={async () => {
-            "use server";
-            await signIn("google");
-          }}
-        >
-          <button type="submit" className={styles.gsiMaterialButton}>
-            <div className={styles.gsiMaterialButtonState}></div>
-
-            <div className={styles.gsiMaterialButtonContentWrapper}>
-              <div className={styles.gsiMaterialButtonIcon}>
-                <img
-                  src="/googleicon.svg"
-                  alt="Google icon"
-                  width={80}
-                  height={80}
-                />
-              </div>
-
-              <span className={styles.gsiMaterialButtonContents}>
-               Googleでログインして始める。
-              </span>
+          <div className={styles.gsiMaterialButtonContentWrapper}>
+            <div className={styles.gsiMaterialButtonIcon}>
+              <img
+                src="/googleicon.svg"
+                alt="Google icon"
+                width={80}
+                height={80}
+              />
             </div>
-          </button>
-        </form>
-      )}
+
+            <span className={styles.gsiMaterialButtonContents}>
+              Googleでログインして始める。
+            </span>
+          </div>
+        </button>
+      </form>
     </div>
   );
 }
